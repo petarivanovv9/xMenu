69b7b53d91574df7985f80ebdd7f3e62f0c0750d
Petar Ivanov <pepincho95@gmail.com>
2017-02-13 23:10:46 +0200

    Add almost ready ajax-post-functionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,46 +5,28 @@ console.log("Connected!");
 $(document).ready(function(){
   $('#signin-sub').click(function(){
 
-    alert("HERE");
+    alert("before Ajax!");
 
     var email_tmp = $('#email').val();
     var pass = $('#password').val();
 
-  //   $.post('/jquery/submitData',
-  //   {
-  //     email: email_tmp,
-  //     password: pass
-  //   },
-  //   function(data, status){
-  //     if(data == 'success'){
-  //       alert( "success" );
-  //       window.location.href = "index.html";
-  //     } else {
-  //       alert( "error" );
-  //       $('#error').text('error');
-  //     }
-  //     alert("bam");
-  //   }, 'json');
-
-  //  alert("BOOO");
-
-  // });
-
-    // $.get("demo_test.asp", function(data, status){
-    //     alert("Data: " + data + "\nStatus: " + status);
-    // });
-
     $.ajax({
       type: "GET",
       url: "http://petarivanov.me/",
-      // data: { 'submit':true },
+      data: {
+        'submit':true
+      },
       success: function(result) {
-          alert('call back');
+          alert( "success" );
+          window.location.href = "index.html";
       },
       error: function(jqXHR, textStatus, errorThrown) {
-        alert("Error, status = " + textStatus + ", " + "error thrown: " + errorThrown);
+        alert( "error" );
+        // alert("Error, status = " + textStatus + ", " + "error thrown: " + errorThrown);
       }
     });
 
-});
+    alert("after Ajax!");
+
+  });
 });
