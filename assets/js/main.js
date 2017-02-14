6f535dd0312c0f73ba63aaf6fe97ef4205e2a7e5
Petar Ivanov <pepincho95@gmail.com>
2017-02-14 20:21:56 +0200

    Fix get and post requests.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,18 @@
 'use strict';
 
-// console.log("Connected!");
 
 $(document).ready(function(){
   $('#signin-sub').click(function(event){
     event.preventDefault();
-    // alert("before Ajax!");
 
     var email_tmp = $('#email').val();
     var pass = $('#password').val();
 
+    var root = 'https://jsonplaceholder.typicode.com';
+
     $.ajax({
       type: "GET",
-      url: "http://petarivanov.me/",
-      // data: {
-      //   'email': email_tmp,
-      //   'password': pass,
-      //   'submit':true
-      // },
+      url: root + '/posts/1',
       success: function(result) {
           console.log( "success" );
           window.location.href = "index.html";
@@ -27,34 +22,34 @@ $(document).ready(function(){
         console.log("Error, status = " + textStatus + ", " + "error thrown: " + errorThrown);
       }
     });
-
-    // alert("after Ajax!");
-
   });
 
 
   $('#signup-sub').click(function(event){
     event.preventDefault();
-    // alert("before Ajax!");
 
     var email_tmp = $('#email').val();
     var pass = $('#password').val();
     var pass2 = $('#password2').val();
 
-    $.post('http://petarivanov.me/',
+    var root = 'https://jsonplaceholder.typicode.com';
+
+    $.post(root + '/posts',
     {
-      'email': email_tmp,
-      'password': pass,
-      'password2': pass2
+      username: "pepincho",
+      email: email_tmp,
+      password: pass,
+      password2: pass2
     },
     function(data, status){
+      if (status=="success")  {
+        console.log( "success" );
+        window.location.href = "index.html";
+      } else {
+        console.log( "error" );
         console.log('data: ' + data + '\nStatus: ' + status);
+      }
     });
   });
-
-    // alert("after Ajax!");
-
-  // });
-
 
 });
